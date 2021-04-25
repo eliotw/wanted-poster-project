c4ce39c84e356186c83bbc7f69f14fc2d25f1f98
Eliot Wong <>
2021-04-25 00:35:53 +0000

    Final state

--- static/js/main.js
+++ static/js/main.js
@@ -1,48 +1,76 @@
-function addLastSeenElement(event) {
-  const list = document.getElementById("last-seen");
+const lastSeenForm = document.getElementById('last-seen-form');
+lastSeenForm.addEventListener('submit', handleAddRecord);
 
+fetchRecords();
+
+// Helper functions
+function handleAddRecord(event) {
+  event.preventDefault();
+
+  const record = getRecordFromForm(event);
+  
+  saveRecord(record)
+    .then((newRecord) => addLastSeenElement(newRecord));
+
+  // Clear out value from input field
+  document.getElementById('last-seen-location').value="";
+}
+
+function addLastSeenElement(record) {
+  const list = document.getElementById("last-seen-list");
+
+  const item = createLastSeenItem(record);
+  list.appendChild(item);
+}
+
+function createLastSeenItem(record) {
   const item = document.createElement('li');
-  item.id = `last-seen-${event.id}`;
+  item.id = `last-seen-${record.id}`;
 
+  // Add location text to item
   const p = document.createElement('p');
-  p.textContent = event.location;
+  p.textContent = record.location;
   item.appendChild(p);
 
+  // Add a button to delete the item from list
   const button = document.createElement('input');
   button.type = 'image';
   button.src = '/static/img/delete.png';
   button.className = 'delete-button';
   button.onclick = () => {
-    fetch(`/api/events/${event.id}`, {
-      method: 'DELETE'
-    }).then(() => item.remove());
+    deleteRecord(record.id).then(() => item.remove());
   }
   item.append(button);
 
-  list.appendChild(item);
+  return item;
 }
 
-const lastSeenForm = document.getElementById('last-seen-form');
-lastSeenForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  console.log(e.currentTarget);
-  const formData = new FormData(e.currentTarget);
-  const data = Object.fromEntries(formData.entries());
-  const json = JSON.stringify(data);
+function deleteRecord(recordId) {
+  return fetch(`/api/records/${recordId}`, {
+      method: 'DELETE'
+    });
+}
 
-  fetch('/api/events', {
+function saveRecord(record) {
+  return fetch('/api/records', {
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
-    body: json
-  }).then((response) => response.json())
-  .then((newEvent) => addLastSeenElement(newEvent));
-})
+    body: JSON.stringify(record)
+  }).then((response) => response.json());
+}
 
-fetch('/api/events')
-  .then((response) => response.json())
-  .then((json) => {
-    
-    json.forEach((event) => {
-      addLastSeenElement(event);
-    });
-  })
+function getRecordFromForm(event) {
+  const formData = new FormData(event.currentTarget);
+  const data = Object.fromEntries(formData.entries());
+  return data;
+}
+
+function fetchRecords() {
+  fetch('/api/records')
+    .then((response) => response.json())
+    .then((json) => {     
+      json.forEach((record) => {
+        addLastSeenElement(record);
+      });
+    })
+}
